4da530eaf464935b3bbb241b5f2134f7e81939bb
srirams-fw <sriram.s@freshworks.com>
2024-08-16 13:02:43 +0530

    header

--- Mark1/app/controllers/application_controller.rb
+++ Mark1/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    def setLang(_selectedLang)
+        case _selectedLang
+            when "val1"
+                _lang= :en
+            when "val2"
+                _lang= :fr
+            else
+                _lang= :en
+        end
+        I18n.locale = _lang
+    end
+    #redirect_to request.referrer
 end
