a2b00c26199501b0b3ea2dcd63556033986c1a36
srirams-fw <sriram.s@freshworks.com>
2024-08-19 16:09:55 +0530

    ember integration

--- Mark1/config/routes.rb
+++ Mark1/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  mount_ember_app :frontend, to: "/dashboard"
+  #mount_ember_app :frontend, to: "/dashboard", controller: "ember", action: "index", constraints: Workspace::Path::WebPath.new
   resources :tickets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'login#root'
   get '/login' => 'login#loadLogin'
-  get '/dashboard' => 'dashboard#loadDashboard'
   get '/authenticate', to: 'login#authenticate'
   get '/resend_mail', to: 'login#resend_mail'
   get '/authorize/:key', to: 'login#authorize'
