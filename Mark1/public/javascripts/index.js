28d148c45383d774662c58657c5de62316885445
srirams-fw <sriram.s@freshworks.com>
2024-08-22 19:21:05 +0530

    cleanup & debounce

--- Mark1/public/javascripts/index.js
+++ Mark1/public/javascripts/index.js
@@ -9,22 +9,37 @@ function sanitizeHTML(html) {
     return doc.body.innerHTML;
 }
 
+function debounce(func, delay=500) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), delay);
+  };
+}
+
+
+
+
+var apiCall = (url,method='GET',header={},body={},callback=()=>{}) => {
+  fetch(url, {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+      ...header
+    },
+    body: JSON.stringify(body)
+  }) .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error:: Response in ${url} -> ${response.statusText}` );
+    }
+    return response.json();
+  }).then(callback);
+}
+var reload = () => window.location.reload();
 document.addEventListener('DOMContentLoaded', function() {
     var languageSelect = document.getElementById('language-select');
-  
-    languageSelect.addEventListener('change', function() {
-      var selectedLanguage = this.value;
-
-      fetch('/set_language', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-        },
-        body: JSON.stringify({ locale: selectedLanguage })
-      }).then(function() {
-        window.location.reload();
-      });
-    });
+    languageSelect.addEventListener('change', function() { apiCall('/set_language','POST',null,{ locale: this.value },reload)});
   });
   
