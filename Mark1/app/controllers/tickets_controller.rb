7baf70f3761360c2b9f2f9482b5b5fc6f52e6692
srirams-fw <sriram.s@freshworks.com>
2024-08-23 17:28:21 +0530

    assignee and parent ticket

--- Mark1/app/controllers/tickets_controller.rb
+++ Mark1/app/controllers/tickets_controller.rb
@@ -61,6 +61,12 @@ class TicketsController < ApplicationController
     end
   end
 
+  def fetch_tickets
+      _query = params[:query]
+      _result = get_ticket_list(_query)
+    render json: { query:_query,result:_result  }, status: :ok
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_ticket
@@ -72,6 +78,14 @@ class TicketsController < ApplicationController
       params.require(:ticket).permit(:product, :ticket_type, :summary, :reporter, :status, :parent_ticket_id, :environment_found, :description, :priority, :severity, :start_date, :due_date, :assignee, :labels, :fix_version, :attachments, :comments)
     end
 
+    def get_assignee_list
+      return User.pluck(:email)
+    end
+
+    def get_ticket_list(_query='')
+        return Ticket.where("ticket_type in (?,?,?) AND summary like '#{_query}%'",'Task', 'Epic', 'Story').pluck(:summary, :id)
+    end
+
     def set_attribute_values
       @product_options = [['ER', 'ER'], ['ESM', 'ESM'], ['MSP', 'MSP']]
       @ticket_type_options = [['Bug', 'Bug'], ['Defect', 'Defect'], ['Task', 'Task'], ['Epic', 'Epic'], ['Story', 'Story']]
@@ -79,5 +93,6 @@ class TicketsController < ApplicationController
       @environment_found_options = [['Select',''],['QA', 'QA'], ['UAT', 'UAT'], ['PreProd', 'PreProd'], ['Prod', 'Prod']]
       @priority_options = [['P3', 'P3'], ['P2', 'P2'], ['P1', 'P1']]
       @severity_options = [['Low', 'Low'], ['Medium', 'Medium'], ['High', 'High'], ['Urgent', 'Urgent']]
+      @assignee_options = get_assignee_list
     end
 end
