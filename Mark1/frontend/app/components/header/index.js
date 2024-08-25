b56a40fbe5c4ba6ea69d631724ef3208d3cb1cf9
srirams-fw <sriram.s@freshworks.com>
2024-08-25 16:17:10 +0530

    dashboard header

---
+++ Mark1/frontend/app/components/header/index.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import {apiCall} from '../../helpers/index'
+
+export default class HeaderIndexComponent extends Component {
+  
+  @action
+  async handleClick(event) {
+    event.preventDefault(); 
+    await apiCall('/logout','DELETE')
+    window.location.reload(true);
+  }
+}
