88e0951f6abaa11c5c129dff31df378f6c6f67aa
srirams-fw <sriram.s@freshworks.com>
2024-08-27 10:31:13 +0530

    color theme handshake

--- Mark1/frontend/app/components/header/index.js
+++ Mark1/frontend/app/components/header/index.js
@@ -37,5 +37,6 @@ export default class HeaderIndexComponent extends Component {
   @action
   colorThemeToggle(event){
     document.body.classList.toggle('dark-mode')
+    apiCall('/set_theme','POST',null,{ theme: document.body.classList.contains('dark-mode') ? 'dark' : 'light'  })
   }
 }
--- Mark1/frontend/app/controllers/application.js
+++ Mark1/frontend/app/controllers/application.js
@@ -9,12 +9,17 @@ export default class ApplicationController extends Controller {
     super(...arguments);
   }
 
-  setData({ userName, email, locale }) {
+  setData({ userName, email, locale, theme }) {
     this.set('userName', userName);
     this.set('email', email);
     this.set('locale', locale);
+    this.set('theme',theme)
 
     this.translationLoader.loadTranslations(locale); 
-    
+    this.applyTheme(theme);
+  }
+
+  applyTheme(theme) {
+    document.body.classList.add(`${theme == 'dark' ? 'dark-mode' : 'light-mode'}`);
   }
 }
